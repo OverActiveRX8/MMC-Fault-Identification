f812268b04aea02c8ef3d2eed6a95900c5cd7220
zjj <939818121@qq.com>
2019-11-12 23:27:17 +0800

    getdata

--- Other Demos/GetData.py
+++ Other Demos/GetData.py
@@ -1,7 +1,6 @@
-import names as names
+
 import numpy as np
 import xlrd
-import pandas as pd
 
 def dataReader(ncolsBegin, ncolsEnd, nrowsLength, stepNumber, nrowsBegin, setLabel, interval):
     data = xlrd.open_workbook('finaldata.xlsx')
@@ -44,7 +43,7 @@ def dataReader2(ncolsBegin, ncolsEnd, nrowsLength, stepNumber, nrowsBegin, setLa
     useLabel = []
     while True:
         if nrowsBegin % interval < 5:
-            label = [setLabel]
+            label = [0]
             for c in range(nrowsBegin, nrowsBegin + nrowsLength):
                 for r in range(ncolsBegin, ncolsEnd):
                     val = []
@@ -52,7 +51,7 @@ def dataReader2(ncolsBegin, ncolsEnd, nrowsLength, stepNumber, nrowsBegin, setLa
                     data.append(tuple(val))
             useLabel.append(tuple(label))
         else:
-            label = [abs(setLabel - 1)]
+            label = [setLabel]
             for c in range(nrowsBegin, nrowsBegin + nrowsLength):
                 for r in range(ncolsBegin + 5, ncolsEnd + 5):
                     val = []
@@ -66,15 +65,32 @@ def dataReader2(ncolsBegin, ncolsEnd, nrowsLength, stepNumber, nrowsBegin, setLa
     dataNumpyType = np.r_[data]
     data = np.reshape(dataNumpyType, (stepNumber, nrowsLength, ncolsEnd - ncolsBegin), order='C')
     useLabel = np.reshape(labelNumpyYype, (stepNumber, 1), order='C')
-    useLabel = useLabel.astype(np.float64)
+    useLabel = useLabel.astype(np.int8)
     dataLabel = ((data,) + (useLabel,))
     return dataLabel
 names = locals()
 for j in range(1, 9):
     names['trainData' + str(j)] = dataReader2(ncolsBegin=10 * (j - 1) + 6, ncolsEnd=10 * (j - 1) + 10,
-                                              nrowsLength=100, stepNumber=1000, nrowsBegin=0, setLabel=0, interval=10)
+                                              nrowsLength=100, stepNumber=1000, nrowsBegin=0, setLabel=j, interval=10)
     names['testData' + str(j)] = dataReader(ncolsBegin=10 * (j - 1) + 6, ncolsEnd=10 * (j - 1) + 10,
-                                            nrowsLength=100, stepNumber=1000, nrowsBegin=0, setLabel=0, interval=10)
-        #读取好数据，ABCD代表四个子模块的电压数据
-#读取坏数据，一共四个子模块，每个子模块两个ijbt分别故障
+                                            nrowsLength=100, stepNumber=1000, nrowsBegin=0, setLabel=j, interval=10)
 
+for i in range(1, 9):
+    if i == 1:
+        trainD = names['trainData' + str(i)][0]
+        trainL = names['trainData' + str(i)][1]
+        testD = names['testData' + str(i)][0]
+        testL = names['testData' + str(i)][1]
+    else:
+        trainD = np.r_[trainD, names['trainData' + str(i)][0]]
+        trainL = np.r_[trainL, names['trainData' + str(i)][1]]
+        testD = np.r_[testD, names['testData' + str(i)][0]]
+        testL = np.r_[testL, names['testData' + str(i)][1]]
+def trainData():
+    trainData = (trainD, trainL)
+    return trainData
+def testData():
+    testData = (testD, testL)
+    return testData
+tr = trainData()
+ts = testData()
